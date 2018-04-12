34ddf1c54d3bf84c5cb4351a31c39e1541e7c3b6
tockorea <lim.chae.eun@gmail.com>
2018-04-12 22:51:51 +0900

    Lesson04 - Using Computed Properties

---
+++ lesson04/vuex-playlist/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne";
+import ProductListTwo from "./components/ProductListTwo";
+
+export default {
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
