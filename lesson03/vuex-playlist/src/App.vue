cd7288408abe87915e6b7d74dd97e55bbfbef2f7
tockorea <lim.chae.eun@gmail.com>
2018-04-12 22:17:20 +0900

    Lesson03 - Setting up a Central Store

---
+++ lesson03/vuex-playlist/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne";
+import ProductListTwo from "./components/ProductListTwo";
+
+export default {
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
+  name: "app",
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
